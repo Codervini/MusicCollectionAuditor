fdf44a26340645185db00adc14a821ba88f36fe2
Codervini <78258591+Codervini@users.noreply.github.com>
2026-03-19 15:47:08 +0530

    fiixed bug: generate_new_machine_id() json wrong saving

--- tools/machine_identifier.py
+++ tools/machine_identifier.py
@@ -6,48 +6,54 @@ import socket
 import platform
 import datetime
 
-def generate_new_machine_id(path, machine_id_json:dict,id_type:str="system_defined"):
+def generate_new_machine_id(path, full_json:dict, id_type:str="system_defined"):
     '''type = user_defined or system_defined'''
+    machine_id_json = full_json["machine_id"]
+    machine_id = machineid.id()
 
     if id_type == "system_defined":
         id_config = {
                 "name": socket.gethostname(),
                 "type": "system_defined",
-                "current_uuid": machineid.id(),
+                "current_uuid": machine_id,
                 "previous_uuids": [],
                 "os": platform.system(),
                 "first_seen": str(datetime.datetime.now()),
                 "last_seen": ""
             }
-        machine_id_json["saved_machines"][machineid.id()] = id_config
+        machine_id_json["saved_machines"][machine_id] = id_config
     elif id_type == "user_defined":
-        name = str(input("Name of Machine ID: "))
+        machine_id = str(input("Name of Machine ID: "))
 
-        if machine_id_json["saved_machines"].get(name,None):
+        if machine_id_json["saved_machines"].get(machine_id,None):
             print("ID with same name already present")
             choice = int(input('''1) To add current system GUID under same name
                                   2) To skip 
                                     Choice: '''))    
             match choice:
                 case 1:
-                    machine_id_json["saved_machines"][name]["previous_uuids"].append(machineid.id())
+                    machine_id_json["saved_machines"][machine_id]["previous_uuids"].append(machineid.id())
                 case 2:
-                     machine_id_json["saved_machines"][name]["previous_uuids"] = []
+                     machine_id_json["saved_machines"][machine_id]["previous_uuids"] = []
                 case _:
                     print("Invalid option")
             
         id_config = {
-                "name": name,
+                "name": machine_id,
                 "type": "user_defined",
                 "current_uuid": machineid.id(),
                 "os": platform.system(),
                 "first_seen": str(datetime.datetime.now()),
                 "last_seen": ""
             }
-        machine_id_json["saved_machines"][name] = id_config
+        machine_id_json["saved_machines"][machine_id] = id_config
+
+    full_json["machine_id"] = machine_id_json
 
     with open(path,"w") as f:
-        json.dump(machine_id_json,f)
+        json.dump(full_json,f)
+
+    return 
 
     
 
@@ -56,7 +62,8 @@ def machine_id():
     config = Path("data","config.json")
 
     with open(config,"r") as f:
-        machine_id_json = json.load(f)["machine_id"]
+        full_json = json.load(f)
+        machine_id_json = full_json["machine_id"]
     # pprint(machine_id_json)
 
 
@@ -71,8 +78,8 @@ def machine_id():
             print(f"Machine ID not selected, please select a ID or make new")
         else:
             print("No ID available, make new")
-            generate_new_machine_id(config,machine_id_json,"user_defined")
-            return machine_id
+            generate_new_machine_id(config,full_json,"user_defined")
+            return machine_id()
     return None
 # json.dump()
 
