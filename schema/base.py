eb71301d78e81c65161b5db6ba5df16c53fadd1c
Codervini <78258591+Codervini@users.noreply.github.com>
2026-03-31 21:16:04 +0530

    refractor: Base is now a class

--- schema/base.py
+++ schema/base.py
@@ -1,13 +1,15 @@
 import dotenv
 from mca_tools.machine_identifier import  machine_id
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base ,sessionmaker
+from sqlalchemy.orm import DeclarativeBase ,sessionmaker
 
 #----- Constants -----------------------------------------------------------------
 CONFIG_CONSTANTS = dotenv.dotenv_values("/home/vinish/Desktop/MusicCollectionAuditor/data/config/.env")
 DB_ENGINE = create_engine(f"postgresql+psycopg://{CONFIG_CONSTANTS['DB_USERNAME']}:{CONFIG_CONSTANTS['DB_PASSWORD']}@localhost:5432/mcamusicdb")
 SESSION_MANAGER = sessionmaker(bind=DB_ENGINE)
 MACHINE_ID = machine_id()
-Base = declarative_base()
 
 
+
+class Base(DeclarativeBase):
+    pass
