8c8f5beaaf6cfe69a007d3c66bf7cf277203f4fb
Codervini <78258591+Codervini@users.noreply.github.com>
2026-08-05 21:01:25 +0530

    update: api_cacher fine tuning

--- mca_tools/cacher/api_cacher.py
+++ mca_tools/cacher/api_cacher.py
@@ -1,25 +1,24 @@
 from requests_cache import CachedSession
 from pathlib import Path
 from mca.core.logger import set_logger
-from dotenv import dotenv_values
 from datetime import timedelta
 
 logger = set_logger(__name__)
-
+services = {"musicbrainz", "lastfm", "discogs", "spotify"}
 
 CACHE_DIR = Path(".cache/api")
 
-def get_session(service: str, ttl_days: int = 7) -> CachedSession:
-    services = ["musicbrainz", "last.fm"]
+def get_session(service: str, expiry_in_days: int = 7) -> CachedSession:
     if service not in services:
-        print("Invalid service")
         logger.error("Invalid service selected")
-        return
+        raise ValueError(f"Invalid service: {service}")
+    
     CACHE_DIR.mkdir(parents=True, exist_ok=True)
-    logger.info("Cached Session returned")
+    logger.info(f"Cached Session for {service} created")
     return CachedSession(
+        backend="sqlite",
         cache_name=str(CACHE_DIR / service),
-        expire_after=timedelta(days=ttl_days),
+        expire_after=timedelta(days=expiry_in_days),
         ignored_parameters=['api_key', 'client_id', 'client_secret'],
-        stale_if_error=True,
+        stale_if_error=True
     )
