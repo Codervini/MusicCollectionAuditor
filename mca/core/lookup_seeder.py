a886a6246a4d3f96a47c6f70d3427c214084f689
Codervini <78258591+Codervini@users.noreply.github.com>
2026-08-31 23:00:52 +0530

    update: lookup values

--- mca/core/lookup_seeder.py
+++ mca/core/lookup_seeder.py
@@ -64,16 +64,16 @@ def seed_gender_lookup():
 
 def seed_artist_type_lookup():
     artist_types = [
-    ("Person",      "A single individual artist"),
-    ("Group",       "A band, ensemble, or musical group"),
-    ("Choir",       "A vocal ensemble or chorus"),
-    ("Orchestra",   "A large classical or symphonic ensemble"),
-    ("Character",   "A fictional or animated character"),
-    ("Other",       "An artist type that does not fit standard categories"),
-    ("Unknown",     "Artist type not known or not recorded"),
+        ("Person","A single individual artist","b6e035f4-3ce9-331c-97df-83397230b0df","MusicBrainz"),
+        ("Group","A band, ensemble, or musical group","e431f5f6-b5d2-343d-8b36-72607fffb74b","MusicBrainz"),
+        ("Choir","A vocal ensemble or chorus","6124967d-7e3a-3eba-b642-c9a2ffb44d94","MusicBrainz"),
+        ("Orchestra","A large classical or symphonic ensemble","a0b36c92-3eb1-3839-a4f9-4799823f54a5","MusicBrainz"),
+        ("Character","A fictional or animated character","5c1375b0-f18d-3db7-a164-a49d7a63773f","MusicBrainz"),
+        ("Other","An artist type that does not fit standard categories","ac897045-5043-3294-969b-187360e45d86","MusicBrainz"),
+        ("Unknown","Artist type not known or not recorded",None,"Codervini"),
     ]
     for i in artist_types:
-        insert_multiple_columns_data(ArtistTypeLookup,{"name":i[0],"description":i[1]})
+        insert_multiple_columns_data(ArtistTypeLookup,{"name":i[0],"description":i[1],"alt_type_id":i[2],"ingestion_source":i[3]})
     logger.debug("Artist Type Lookup Seeded")
 
 def seed_alias_types_lookup():
@@ -121,83 +121,83 @@ def seed_link_types_lookup():
     link_types = [
 
     # Official
-    ("Official Homepage","https://","Artist's official website"),
-    ("Official Store","https://","Official merch or music store"),
+    ("Official Homepage","https://","Artist's official website","fe33d22f-c3b0-4d68-bd53-a856badf2b15","MusicBrainz"),
+    ("Official Store","https://","Official merch or music store",None,"Codervini"),
 
     # Social Networks
-    ("Facebook","https://www.facebook.com/","Facebook page or profile"),
-    ("Instagram","https://www.instagram.com/","Instagram profile"),
-    ("Twitter","https://twitter.com/","Twitter/X profile"),
-    ("TikTok","https://www.tiktok.com/@","TikTok profile"),
-    ("Myspace","https://myspace.com/","Myspace page"),
-    ("SoundCloud","https://soundcloud.com/","SoundCloud profile"),
-    ("Snapchat","https://www.snapchat.com/add/","Snapchat profile"),
-    ("Threads","https://www.threads.net/@","Threads profile"),
-    ("Bluesky","https://bsky.app/profile/","Bluesky profile"),
-    ("Mastodon","https://","Mastodon profile"),
-    ("Tumblr","https://","Tumblr blog"),
+    ("Facebook","https://www.facebook.com/","Facebook page or profile",None,"Codervini"),
+    ("Instagram","https://www.instagram.com/","Instagram profile", None,"Codervini"),
+    ("Twitter","https://twitter.com/","Twitter/X profile",None,"Codervini"),
+    ("TikTok","https://www.tiktok.com/@","TikTok profile",None,"Codervini"),
+    ("Myspace","https://myspace.com/","Myspace page","bac47923-ecde-4b59-822e-d08f0cd10156","MusicBrainz"),
+    ("SoundCloud","https://soundcloud.com/","SoundCloud profile","89e4a949-0976-440d-bda1-5f772c1e5710","MusicBrainz"),
+    ("Snapchat","https://www.snapchat.com/add/","Snapchat profile",None,"Codervini"),
+    ("Threads","https://www.threads.net/@","Threads profile",None,"Codervini"),
+    ("Bluesky","https://bsky.app/profile/","Bluesky profile",None,"Codervini"),
+    ("Mastodon","https://","Mastodon profile",None,"Codervini"),
+    ("Tumblr","https://","Tumblr blog",None,"Codervini"),
 
     # Video
-    ("YouTube","https://www.youtube.com/","Official YouTube channel"),
-    ("YouTube Music","https://music.youtube.com/","YouTube Music channel"),
-    ("Vimeo","https://vimeo.com/","Vimeo channel"),
+    ("YouTube","https://www.youtube.com/","Official YouTube channel","6a540e5b-58c6-4192-b6ba-dbc71ec8fcf0","MusicBrainz"),
+    ("YouTube Music","https://music.youtube.com/","YouTube Music channel","631712a0-7525-42ba-b7a3-605aa7a238c4","MusicBrainz"),
+    ("Vimeo","https://vimeo.com/","Vimeo channel",None,"Codervini"),
 
     # Streaming
-    ("Spotify","https://open.spotify.com/artist/","Spotify artist page"),
-    ("Apple Music","https://music.apple.com/","Apple Music artist page"),
-    ("Tidal","https://tidal.com/browse/artist/","Tidal artist page"),
-    ("Deezer","https://www.deezer.com/en/artist/","Deezer artist page"),
-    ("Amazon Music","https://music.amazon.com/artists/","Amazon Music artist page"),
-    ("Pandora","https://www.pandora.com/artist/","Pandora artist page"),
+    ("Spotify","https://open.spotify.com/artist/","Spotify artist page",None,"Codervini"),
+    ("Apple Music","https://music.apple.com/","Apple Music artist page","64785d6c-2eeb-4f86-9418-b6c2d6c53c13","MusicBrainz"),
+    ("Tidal","https://tidal.com/browse/artist/","Tidal artist page", None,"Codervini"),
+    ("Deezer","https://www.deezer.com/en/artist/","Deezer artist page",None,"Codervini"),
+    ("Amazon Music","https://music.amazon.com/artists/","Amazon Music artist page",None,"Codervini"),
+    ("Pandora","https://www.pandora.com/artist/","Pandora artist page",None,"Codervini"),
 
     # Purchase / Download
-    ("Bandcamp","https://","Bandcamp artist page"),
-    ("CD Baby","https://store.cdbaby.com/Artist/","CD Baby artist page"),
-    ("iTunes","https://music.apple.com/","iTunes artist page"),
-    ("Purchase Download","https://","Page where music can be purchased for download"),
-    ("Purchase Mail Order","https://","Page where music can be purchased by mail order"),
-    ("Free Download","https://","Page where music can be downloaded for free"),
-    ("Free Streaming","https://","Page where music can be streamed for free"),
+    ("Bandcamp","https://","Bandcamp artist page","c550166e-0548-4a18-b1d4-e2ae423a3e88","MusicBrainz"),
+    ("CD Baby","https://store.cdbaby.com/Artist/","CD Baby artist page","4c21e5f5-2960-4abc-88a1-62ce491bb96e","MusicBrainz"),
+    ("iTunes","https://music.apple.com/","iTunes artist page",None,"Codervini"),
+    ("Purchase Download","https://","Page where music can be purchased for download","f8319a2f-f824-4617-81c8-be6560b3b203","MusicBrainz"),
+    ("Purchase Mail Order","https://","Page where music can be purchased by mail order","611b1862-67af-4253-a64f-34adba305d1d","MusicBrainz"),
+    ("Free Download","https://","Page where music can be downloaded for free","34ae77fe-defb-43ea-95d4-63c7540bac78","MusicBrainz"),
+     ("Free Streaming","https://","Page where music can be streamed for free","769085a1-c2f7-4c24-a532-2375a77693bd","MusicBrainz"),
 
     # Databases / Reference
-    ("MusicBrainz","https://musicbrainz.org/artist/","MusicBrainz artist page"),
-    ("Discogs","https://www.discogs.com/artist/","Discogs artist page"),
-    ("Last.fm","https://www.last.fm/music/","Last.fm artist page"),
-    ("AllMusic","https://www.allmusic.com/artist/","AllMusic artist page"),
-    ("Wikidata","https://www.wikidata.org/wiki/","Wikidata entity page"),
-    ("Wikipedia","https://en.wikipedia.org/wiki/","Wikipedia article"),
-    ("IMDb","https://www.imdb.com/name/","IMDb page"),
-    ("IMSLP","https://imslp.org/wiki/","IMSLP page for classical works"),
-    ("SecondHandSongs","https://secondhandsongs.com/artist/","SecondHandSongs page"),
-    ("Setlist.fm","https://www.setlist.fm/setlists/","Setlist.fm artist page"),
-    ("Songkick","https://www.songkick.com/artists/","Songkick artist page"),
-    ("Bandsintown","https://www.bandsintown.com/","Bandsintown artist page"),
-    ("VGMdb","https://vgmdb.net/artist/","VGMdb page for video game/anime music artists"),
-    ("VIAF","https://viaf.org/viaf/","Virtual International Authority File ID"),
-    ("CPDL","https://www.cpdl.org/wiki/index.php/","Choral Public Domain Library page"),
-    ("BookBrainz","https://bookbrainz.org/creator/","BookBrainz page"),
+    ("MusicBrainz","https://musicbrainz.org/artist/","MusicBrainz artist page",None,"Codervini"),
+    ("Discogs","https://www.discogs.com/artist/","Discogs artist page","04a5b104-a4c2-4bac-99a1-7b837c37d9e4","MusicBrainz"),
+    ("Last.fm","https://www.last.fm/music/","Last.fm artist page","08db8098-c0df-4b78-82c3-c8697b4bba7f","MusicBrainz"),
+    ("AllMusic","https://www.allmusic.com/artist/","AllMusic artist page","6b3e3c85-0002-4f34-aca6-80ace0d7e846","MusicBrainz"),
+    ("Wikidata","https://www.wikidata.org/wiki/","Wikidata entity page","689870a4-a1e4-4912-b17f-7b2664215698","MusicBrainz"),
+    ("Wikipedia","https://en.wikipedia.org/wiki/","Wikipedia article","29651736-fa6d-48e4-aadc-a557c6add1cb","MusicBrainz"),
+    ("IMDb","https://www.imdb.com/name/","IMDb page","94c8b0cc-4477-4106-932c-da60e63de61c","MusicBrainz"),
+    ("IMSLP","https://imslp.org/wiki/","IMSLP page for classical works","8147b6a2-ad14-4ce7-8f0a-697f9a31f68f","MusicBrainz"),
+    ("SecondHandSongs","https://secondhandsongs.com/artist/","SecondHandSongs page","79c5b84d-a206-4f4c-9832-78c028c312c3","MusicBrainz"),
+    ("Setlist.fm","https://www.setlist.fm/setlists/","Setlist.fm artist page","bf5d0d5e-27a1-4e94-9df7-3cdc67b3b207","MusicBrainz"),
+    ("Songkick","https://www.songkick.com/artists/","Songkick artist page","aac9c4bc-a5b9-30b8-9839-e3ac314c6e58","MusicBrainz"),
+    ("Bandsintown","https://www.bandsintown.com/","Bandsintown artist page","ea45ed3d-2d5e-456e-8c32-94b6f51426e2","MusicBrainz"),
+    ("VGMdb","https://vgmdb.net/artist/","VGMdb page for video game/anime music artists","0af15ab3-c615-46d6-b95b-a5fcd2a92ed9","MusicBrainz"),
+    ("VIAF","https://viaf.org/viaf/","Virtual International Authority File ID","e8571dcc-35d4-4e91-a577-a3382fd84460","MusicBrainz"),
+    ("CPDL","https://www.cpdl.org/wiki/index.php/","Choral Public Domain Library page","991d7d60-01ee-41de-9b62-9ef3f86c2447","MusicBrainz"),
+    ("BookBrainz","https://bookbrainz.org/creator/","BookBrainz page","f82f9342-a08d-46b7-ab7a-d8b6330c805d","MusicBrainz"),
 
     # Content
-    ("Lyrics Page","https://","Page containing lyrics for the artist"),
-    ("Blog","https://","Artist blog"),
-    ("Biography","https://","Online biography of the artist"),
-    ("Discography Page","https://","Online discography of the artist's works"),
-    ("Interview","https://","URL containing an interview with the artist"),
-    ("Image","https://","A pictorial image of the artist"),
-    ("Fan Page","https://","Fan-created website for the artist"),
-    ("Online Community","https://","Online community or forum for the artist"),
-    ("Art Gallery", "https://","Art gallery page e.g. DeviantArt, pixiv"),
+    ("Lyrics Page","https://","Page containing lyrics for the artist","e4d73442-3762-45a8-905c-401da65544ed","MusicBrainz"),
+    ("Blog","https://","Artist blog","eb535226-f8ca-499d-9b18-6a144df4ae6f","MusicBrainz"),
+    ("Biography","https://","Online biography of the artist","78f75830-94e1-4138-8f8a-643e3bb21ce5","MusicBrainz"),
+    ("Discography Page","https://","Online discography of the artist's works","4fb0eeec-a6eb-4ae3-ad52-b55765b94e8f","MusicBrainz"),
+    ("Interview","https://","URL containing an interview with the artist","1f171391-1f98-4f45-b191-038ec3b12395","MusicBrainz"),
+    ("Image","https://","A pictorial image of the artist","221132e9-e30e-43f2-a741-15afc4c5fa7c","MusicBrainz"),
+    ("Fan Page","https://","Fan-created website for the artist","f484f897-81cc-406e-96f9-cd799a04ee24","MusicBrainz"),
+    ("Online Community","https://","Online community or forum for the artist","35b3a50f-bf0e-4309-a3b4-58eeed8cee6a","MusicBrainz"),
+    ("Art Gallery","https://","Art gallery page e.g. DeviantArt, pixiv","8203341a-27be-40bb-b755-08d8ca9d7a9c","MusicBrainz"),
 
     # Funding / Tickets
-    ("Crowdfunding","https://","Crowdfunding page e.g. Kickstarter, Indiegogo"),
-    ("Patronage","https://","Patronage/donation page e.g. Patreon, PayPal.me"),
-    ("Ticketing","https://","Ticket purchase page for events"),
+    ("Crowdfunding","https://","Crowdfunding page e.g. Kickstarter, Indiegogo","93883cf6-e818-4938-990e-75863f8db2d3","MusicBrainz"),
+    ("Patronage","https://","Patronage/donation page e.g. Patreon, PayPal.me","6f77d54e-1d81-4e1a-9ea5-37947577151b","MusicBrainz"),
+    ("Ticketing","https://","Ticket purchase page for events","34beaf28-cbdd-4bf7-bc41-e7de18135245","MusicBrainz"),
 
     # Fallback
-    ("Other","https://","External link that does not fit any other category"),
+    ("Other","https://","External link that does not fit any other category",None,"Codervini"),
 ]
     for i in link_types:
-            insert_multiple_columns_data(LinkTypeLookup,{"name":i[0],"base_url":i[1],"description":i[2],"ingestion_source":"Claude"})
+            insert_multiple_columns_data(LinkTypeLookup,{"name":i[0],"base_url":i[1],"description":i[2],"alt_type_id":i[3],"ingestion_source":i[4]})
     logger.info("Links Type Lookup Seeded")
 
 
