2d446066be5928ce1617b6217d741d8fc1a3225b
Codervini <78258591+Codervini@users.noreply.github.com>
2026-09-05 00:56:15 +0530

    update: implement retry logic for api handler

--- mca_tools/utils.py
+++ mca_tools/utils.py
@@ -3,27 +3,45 @@ import requests
 from mca.core.logger import set_logger
 from datetime import date
 import time
+from requests_cache import CachedSession
+
 logger = set_logger(__name__)
 
 
 def generate_uuidv7():
     return uuid7()
 
-def api_request_handler(api, session, header=None):
-    match header:
-        case None:
-            response = session.get(api)
-        case _:
-            response = session.get(api, headers=header)
-    logger.info(f"API response: {response} | cached={response.from_cache}")
+def api_request_handler(api, session:CachedSession, header=None, retries=5):
+    for attempt in range(1, retries + 1):
+        try:
+            match header:
+                case None:
+                    response = session.get(api)
+                case _:
+                    response = session.get(api, headers=header)
 
-    if response.status_code == 200:
-        data = response.json()
-        logger.debug("Response data: %s", data) 
-        if not response.from_cache: # Only rate-limit actual network requests
-            time.sleep(1.5)
-        return data
-    response.raise_for_status()
+            logger.info(f"API response: {response} | status={response.status_code} | cached={response.from_cache}")
+
+            if response.status_code == 200:
+                data = response.json()
+                logger.debug(f"Response data: {data}")
+                if session.service == "musicbrainz" and not response.from_cache:
+                    time.sleep(2)
+                return data
+            
+            logger.warning(f"API request failed | attempt={attempt}/{retries} | status={response.status_code} | url={api}")
+
+            if response.status_code == 503:
+                time.sleep(5)
+                continue
+            response.raise_for_status()
+        except requests.exceptions.ConnectionError as e:
+            logger.warning(f"Connection error | attempt={attempt}/{retries} | url={api} | error={e}",exc_info=True)
+            if attempt < retries:
+                time.sleep(5)
+                continue
+            raise
+    raise RuntimeError(f"API request failed after {retries} attempts: {api}")
     
 def coerce_to_date(value: str | None) -> date | None:
     if not value:
