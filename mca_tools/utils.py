ade30795816e7660413a2f5ca93caeecf3b07cac
Codervini <78258591+Codervini@users.noreply.github.com>
2026-09-04 23:51:41 +0530

    update: rate limit for new api calls

--- mca_tools/utils.py
+++ mca_tools/utils.py
@@ -2,6 +2,7 @@ from uuid_extensions import uuid7, uuid7str
 import requests
 from mca.core.logger import set_logger
 from datetime import date
+import time
 logger = set_logger(__name__)
 
 
@@ -14,13 +15,15 @@ def api_request_handler(api, session, header=None):
             response = session.get(api)
         case _:
             response = session.get(api, headers=header)
+    logger.info(f"API response: {response} | cached={response.from_cache}")
 
-    logger.debug("API response: %s", response)
     if response.status_code == 200:
         data = response.json()
-        logger.debug("Response data: %s", data)
+        logger.debug("Response data: %s", data) 
+        if not response.from_cache: # Only rate-limit actual network requests
+            time.sleep(1.5)
         return data
-
+    response.raise_for_status()
     
 def coerce_to_date(value: str | None) -> date | None:
     if not value:
